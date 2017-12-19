d295a1b64d4f53da7de3ae542f28eb11a189aea3
WIN-L8NDJSTB3FO\Administrator <475297255@qq.com>
2017-12-19 18:44:14 +0800

    哦豁

--- config.lua
+++ config.lua
@@ -1,21 +1,15 @@
-root = "./skynet/"
+include "config.path"
+
+-- preload = "./examples/preload.lua"	-- run preload.lua before every lua service run
 thread = 8
-harbor = 0
 logger = nil
 logpath = "."
+harbor = 1
+address = "127.0.0.1:2526"
+master = "127.0.0.1:2013"
 start = "main"	-- main script
 bootstrap = "snlua bootstrap"	-- The service for bootstrap
-luaservice = root.."service/?.lua;"..root.."test/?.lua;"..root.."examples/?.lua"
-lualoader = root .. "lualib/loader.lua"
-lua_path = "./mjlib/?.lua;"..root.."lualib/?.lua;"..root.."lualib/?/init.lua"
-lua_cpath = root .. "luaclib/?.so"
--- preload = "./examples/preload.lua"	-- run preload.lua before every lua service run
-snax = root.."examples/?.lua;"..root.."test/?.lua"
+standalone = "0.0.0.0:2013"
 -- snax_interface_g = "snax_g"
 cpath = root.."cservice/?.so"
 -- daemon = "./skynet.pid"
-
---our path
-luaservice = "./service/?.lua;./service/?/main.lua;"..luaservice
-lua_path = "./lualib/?.lua;"..lua_path
-lua_cpath = "./luaclib/?.so;"..lua_cpath
