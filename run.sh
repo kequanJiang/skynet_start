ba626cd6d856e67610759de67ae288c2ff247605
DESKTOP-IIJVCMA\Nil <475297255@qq.com>
2018-10-11 17:14:25 +0800

    reset project

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./skynet/skynet config.lua
--- skynet/my_workspace/test.lua
+++
@@ -1,6 +0,0 @@
-local skynet = require "skynet"
-
-skynet.start(function()
-	--调用skynet.start接口,并定义传入回调函数
-	skynet.error("Server First Test")
-end)
